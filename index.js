073729d0c81e07f13567c486f6407acb1917de39
Keagan McClelland <keagan.mcclelland@gmail.com>
2018-03-22 14:11:43 -0600

    updated compiled code

--- index.js
+++ index.js
@@ -2,9 +2,11 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const jwt = require("express-jwt");
 const jwksClient = require("jwks-rsa");
-function default_1(poolId, clientId) {
-    // https://cognito-idp.us-west-2.amazonaws.com/us-west-2_HOVCUPEvz/.well-known/jwks.json
+function default_1(poolId, property) {
     const region = poolId.split('_')[0];
+    if (!region) {
+        throw new Error('Invalid Pool Id');
+    }
     const url = `https://cognito-idp.${region}.amazonaws.com/${poolId}/.well-known/jwks.json`;
     return jwt({
         secret: jwksClient.expressJwtSecret({
@@ -12,7 +14,7 @@ function default_1(poolId, clientId) {
             rateLimit: true,
             jwksUri: url
         }),
-        audience: clientId,
+        requestProperty: property,
         issuer: `https://cognito-idp.${region}.amazonaws.com/${poolId}`,
         algorithms: ['RS256']
     });
